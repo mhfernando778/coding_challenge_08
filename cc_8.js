ed93c50996bff0dd55cee3f510875ccd1210e3c0
Fernando <mhfernando778@gmail.com>
2025-02-16 20:56:39 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -11,10 +11,23 @@ console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15)}`); // logging the
 
 // Task 2 - Product Price After Discount //
 
-let calculateDiscount = function(price, discountRate) { // starting a function
+const calculateDiscount = function(price, discountRate) { // starting a function
     let finalPrice = price - (price * discountRate); // delcaring equation being used
     return finalPrice.toFixed(2); // returning the output to two decimal places
 }
 
 console.log(`Final Price: $${calculateDiscount(100, 0.2)}`); // logging the calculation with the given values
 console.log(`Final Price: $${calculateDiscount(250, 0.15)}`); // logging the calculation with the given values
+
+
+// Task 3 - Service Fee Calcualtion //
+
+const calculateServiceFee = (amount, serviceType) => amount * serviceType; // starting the arrow function
+const serviceType = { // the different service types and the fees for each one
+    premium: .15,
+    standard: .1,
+    basic: .05
+};
+
+console.log(`Service Fee: $${calculateServiceFee(200, serviceType.premium).toFixed(2)}`);
+console.log(`Service Fee: $${calculateServiceFee(500, serviceType.standard).toFixed(2)}`);
